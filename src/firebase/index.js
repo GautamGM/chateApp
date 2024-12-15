954c16754a19b96d9253c674026d3d2f5820d69b
GautamGM <gautam.netweb@gmail.com>
2024-12-15 19:14:27 +0530

    no bommiy

---
+++ src/firebase/index.js
@@ -0,0 +1,9 @@
+export const config={
+    AppKey:import.meta.env.VITE_API_KEY,
+    AuthDomain:import.meta.env.VITE_AUTH_DOMAIN,
+    Project_id:import.meta.env.VITE_PROJECT_ID,
+    StorageBuckey:import.meta.env.VITE_STORAGE_BUCKET,
+    MessagingSenderId:import.meta.env.VITE_MESSAGE_SENDER_ID,
+    Appid:import.meta.env.VITE_APP_ID,
+    MeasurementId:import.meta.env.VITE_MESUREMENT_ID
+}
