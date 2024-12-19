108cd2441cc49d17494de98252ed28ce185083b5
GautamGM <gautam.netweb@gmail.com>
2024-12-19 23:19:39 +0530

    strikmood add

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <Provider store={store}>
       <App />
       <ToastContainer autoClose={1000} />
     </Provider>
+  </StrictMode>
 );
